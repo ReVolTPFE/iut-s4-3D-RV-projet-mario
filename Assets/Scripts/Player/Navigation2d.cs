15e5f40b32b0555577f460f363a328b9079f9672
Arnaud Steiner <arnaudsteiner@orange.fr>
2022-03-10 12:14:03 +0100

    scene chateau zone créée

--- Assets/Scripts/Player/Navigation2d.cs
+++ Assets/Scripts/Player/Navigation2d.cs
@@ -7,23 +7,41 @@ public class Navigation2d : MonoBehaviour
     
     public GameObject Player;
     public Camera cameraFocusPlayer;
-    public float acceleration = 0.005f;
+    public float acceleration = 0.001f;
     public float maxSpeed = 0.03f;
     public float memoryAcceleration = 0;
-    public int jumpForce = 500;
 
-    public float fallMultiplier = 2.5f;
-    public float lowJumpMultiplier = 2f;
+    public bool isGrounded;
+    public Vector3 jump;
+    public float jumpForce = 3.0f;
+    Rigidbody rb;
+
+    private Vector3 coordY;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         memoryAcceleration = acceleration;
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        coordY = Player.transform.position;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+            isGrounded = true;
+        } else
+        {
+            isGrounded = false;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
         float rotateX = 5 * Input.GetAxis("Mouse X");
         float rotateY = 5 * Input.GetAxis("Mouse Y");
         Player.transform.Rotate(0, rotateX, 0);
@@ -40,26 +58,46 @@ public class Navigation2d : MonoBehaviour
             acceleration = memoryAcceleration;
         }
 
+        if (!Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.D))
+        {
+            Player.GetComponent<Animator>().SetBool("move", false);
+        }
+
         if (Input.GetKey(KeyCode.Z))
         {
+            Player.GetComponent<Animator>().SetBool("move", true);
             Player.transform.position += transform.forward * acceleration;
         }
         if (Input.GetKey(KeyCode.S))
         {
+            Player.GetComponent<Animator>().SetBool("move", true);
+
             Player.transform.position -= transform.forward * acceleration;
         }
         if (Input.GetKey(KeyCode.D))
         {
+            Player.GetComponent<Animator>().SetBool("move", true);
+
             Player.transform.Translate(Vector3.right * acceleration);
         }
         if (Input.GetKey(KeyCode.Q))
         {
+            Player.GetComponent<Animator>().SetBool("move", true);
+
             Player.transform.Translate(Vector3.left * acceleration);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            Player.transform.position += (Vector3.up * jumpForce * Time.deltaTime);
+            if(Player.transform.position != coordY)
+            {
+                isGrounded = false;
+                Player.GetComponent<Animator>().SetTrigger("jump");
+                rb.velocity = Vector3.up * jumpForce;
+            }
+
+            coordY = Player.transform.position;
+
         }
     }
 }
